99af237cf731085a4c90907004fcb48aa906f8fe
saleh <saa111954@hotmail.com>
2023-04-13 21:53:24 +0300

    update: setup registration form

--- client-side/src/app/app-routing.module.ts
+++ client-side/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', loadChildren: () => import('./view/registration/registration.module').then(m => m.RegistrationModule)},
+  {path: 'dashboard', loadChildren: () => import('./view/dashboard/dashboard.module').then(m => m.DashboardModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
