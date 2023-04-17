3b71a21ff745356b8a0f7d8581e0af477b7a5043
saleh alsaggaf <saa111954@hotmail.com>
2023-04-17 04:12:22 +0300

    update: created and added message dialog

---
+++ client-side/src/app/view/dashboard/dialogs/message-dialog/message-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-message-dialog',
+  templateUrl: './message-dialog.component.html',
+  styleUrls: ['./message-dialog.component.scss']
+})
+export class MessageDialogComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<MessageDialogComponent>,
+  ) { }
+
+  ngOnInit(): void {
+  }
+  close() {
+    this.dialogRef.close()
+  }
+  action() {
+    this.data.callback();
+    this.close();
+  }
+}
